cf64d6c9b2efae7fcee3c580cbf36e4c9f689aaa
Matt DuBois <mattdubois2@gmail.com>
2015-04-22 16:19:15 -0500

    add devise and generate devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.preview_path = "#{Rails.root}/lib/mailer_previews"
+  config.action_mailer.asset_host = "http://localhost:3000"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +42,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.airbrake = false
+    Bullet.add_footer = true
+  end
 end
