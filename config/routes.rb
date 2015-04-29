e8615f929b30b8819ebcf32b48efafdb10d15cdb
Matt DuBois <mattdubois2@gmail.com>
2015-04-29 14:46:07 -0500

    Config letter_opener and ensure forgot password? link works correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
 
   root to: "home#index"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
