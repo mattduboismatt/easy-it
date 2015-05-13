e12f8773b347237f4feae3ef0866508adfdc2bb0
Matt DuBois <mattdubois2@gmail.com>
2015-05-13 17:15:15 -0500

    Style up the ticket index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def formatted_time(timestamp)
+    Time.use_zone("Central Time (US & Canada)") do
+      Time.zone.at(timestamp).strftime("%B #{timestamp.day.ordinalize}, %Y")
+    end
+  end
 end
